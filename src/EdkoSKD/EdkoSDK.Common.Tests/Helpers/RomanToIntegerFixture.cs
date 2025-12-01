a028528d63be83f83a98a70c76b0a0923862d8c5
iColt <eduard.kobzik@gmail.com>
2025-12-01 20:40:31 +0100

    [RomanToInteger] Helper method created + tests

---
+++ src/EdkoSKD/EdkoSDK.Common.Tests/Helpers/RomanToIntegerFixture.cs
@@ -0,0 +1,19 @@
+﻿using EdkoSKD.Common.Helpers;
+
+namespace EdkoSDK.Common.Tests.Helpers;
+
+[TestFixture]
+public class RomanToIntegerFixture
+{
+
+    [TestCase("MCMXCIV", 1994)]
+    [TestCase("I", 1)]
+    [TestCase("II", 2)]
+    [TestCase("III", 3)]
+    [TestCase("IV", 4)]
+    [TestCase("LVIII", 58)]
+    public void Test_RomanToInt(string s, int number)
+    {
+        Assert.That(s.ConvertFromRomanToInteger(), Is.EqualTo(number));
+    }
+}
---
+++ src/EdkoSKD/EdkoSKD.Common/Helpers/RomanToInteger.cs
@@ -0,0 +1,41 @@
+﻿namespace EdkoSKD.Common.Helpers;
+
+public static class RomanToInteger
+{
+    public static int ConvertFromRomanToInteger(this string s)
+    {
+        if(s.AsSpan().IndexOfAnyExcept("IVXLCDM") != -1)
+        {
+            throw new ArgumentException($"String <{s}> is not a Roman Integer");
+        }
+
+        var map = new Dictionary<char, int>
+        {
+            ['I'] = 1,
+            ['V'] = 5,
+            ['X'] = 10,
+            ['L'] = 50,
+            ['C'] = 100,
+            ['D'] = 500,
+            ['M'] = 1000
+        };
+
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int value = map[s[i]];
+
+            if (i + 1 < s.Length && map[s[i + 1]] > value)
+            {
+                total -= value;
+            }
+            else
+            {
+                total += value;
+            }
+        }
+
+        return total;
+    }
+}
