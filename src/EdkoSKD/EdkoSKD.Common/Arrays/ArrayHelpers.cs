b30564a39dab21a604e255e89d90d3f11197958d
iColt <eduard.kobzik@gmail.com>
2025-12-04 19:03:50 +0100

    [Tech] Moved some staff from LeetCode proj

---
+++ src/EdkoSKD/EdkoSKD.Common/Arrays/ArrayHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace EdkoSKD.Common.Arrays;
+
+public static class ArrayHelpers
+{
+    public static bool AreEquivalentDistinct(IList<int> a, IList<int> b)
+    {
+        if (a.Count != b.Count)
+        {
+            return false;
+        }
+
+        var sortedA = a.OrderBy(x => x).ToArray();
+        var sortedB = b.OrderBy(x => x).ToArray();
+
+        return sortedA.SequenceEqual(sortedB);
+    }
+
+}
