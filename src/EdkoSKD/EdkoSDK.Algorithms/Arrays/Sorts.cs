adab93cc151be288d048c6c02092f3aa1c24125e
iColt <eduard.kobzik@gmail.com>
2025-12-07 14:18:05 +0100

    [Merge sort] WIP

---
+++ src/EdkoSKD/EdkoSDK.Algorithms/Arrays/Sorts.cs
@@ -0,0 +1,67 @@
+﻿namespace EdkoSDK.Algorithms.Arrays;
+
+public static class Sorts
+{
+    #region Merge Sort
+
+    public static void MergeSort(this int[] array)
+    {
+        // Merge sorted chuncks 2^n, n = [1...x] x = length / 2
+        MergeSortImpl(array);
+    }
+
+    private static void MergeSortImpl(int[] array)
+    {
+        int chunkSize = 1;
+
+        while(chunkSize < array.Length)
+        {
+            for(int i = 0; i < array.Length; i+=chunkSize*2)
+            {
+                MergeChunck(array, chunkSize, i);
+            }
+
+            chunkSize *= 2;
+        }
+    }
+
+    private static void MergeChunck(int[] array, int chunkSize, int startPos)
+    {
+        int firstChunkPointer = startPos;
+        int secondChunkPointer = startPos + chunkSize;
+
+        if(secondChunkPointer >= array.Length)
+        {
+            return;
+        }
+
+        int operationCounter = 0;
+
+        // use temp array as sort in one array is difficult
+        int[] tempArr = new int[chunkSize * 2];
+
+        //while(operationCounter < chunkSize)
+        //{
+        //    if(secondChunkPointer >= array.Length || firstChunkPointer > startPos + chunkSize || secondChunkPointer > startPos + chunkSize * 2)
+        //    {
+        //        break;
+        //    }
+
+        //    if (array[firstChunkPointer] <= array[secondChunkPointer])
+        //    {
+        //        firstChunkPointer++;
+        //    } else
+        //    {
+        //        int temp = array[secondChunkPointer];
+        //        array[secondChunkPointer] = array[firstChunkPointer];
+        //        array[firstChunkPointer] = temp;
+        //        secondChunkPointer++;
+        //    }
+
+        //    operationCounter++;
+            
+        //}
+    }
+
+    #endregion
+}
---
+++ src/EdkoSKD/EdkoSKD.Algorithms.Tests/Arrays/SortsFixture.cs
@@ -0,0 +1,19 @@
+﻿using EdkoSDK.Algorithms.Arrays;
+
+namespace EdkoSKD.Algorithms.Tests.Arrays;
+
+[TestFixture]
+public class SortsFixture
+{
+    [TestCase(new[] {1, 10, 4, 8, 0, -5, 1})]
+    public void Test_Sorts(int[] array)
+    {
+        int[] sortedArr = new int[array.Length];
+        Array.Copy(array, sortedArr, array.Length);
+        Array.Sort(sortedArr);
+
+        Sorts.MergeSort(array);
+
+        CollectionAssert.AreEqual(sortedArr, array);
+    }
+}
