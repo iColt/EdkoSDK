e8a5e4e96123997131c261d04baddd661295265d
iColt <eduard.kobzik@gmail.com>
2026-01-03 12:42:59 +0100

    [Tech] Char extension added

---
+++ src/EdkoSKD/EdkoSKD.Common/Helpers/CharExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace EdkoSKD.Common.Helpers;
+
+public static class CharExtensions
+{
+    public static bool IsPalindrome(this char[] chars, int left, int right)
+    {
+        while (left < right)
+        {
+            if (chars[left] != chars[right])
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
