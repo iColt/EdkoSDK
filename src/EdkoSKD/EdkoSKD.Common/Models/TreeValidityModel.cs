e10cf543a7841842df4a8357ff16fef5b424ff45
iColt <eduard.kobzik@gmail.com>
2026-01-15 21:42:40 +0100

    [Tree] TreeValidityModel added

---
+++ src/EdkoSKD/EdkoSKD.Common/Models/TreeValidityModel.cs
@@ -0,0 +1,19 @@
+﻿namespace EdkoSKD.Common.Models;
+
+public class TreeValidityModel
+{
+    public int? MinValue { get;set; }
+    public int? MaxValue { get;set; }
+
+    public bool IsTreeValid { get; set; }
+
+    public static TreeValidityModel New(int? minValue, int? maxValue, bool isValid = true)
+    {
+        return new TreeValidityModel
+        {
+            MaxValue = maxValue,
+            IsTreeValid = isValid,
+            MinValue = minValue
+        };
+    }
+}
